8da412a0af750644aba6f2fcf0b50b4e18680d4b
mike.zhang <mike.zhang@yeamoney.cn>
2017-11-30 16:30:19 +0800

    sh change

--- deploy.sh
+++ deploy.sh
@@ -60,7 +60,7 @@ if [ -d "./dist" ];
 	then 
 	# backup dest
 	echo -e "==== dest backup ==="
-	mv $DEST_PATH$1/dist $DEST_PATH/dist.bak
+	mv $DEST_PATH$1/dist $DEST_PATH$1/dist.bak
 
 	# copy
 	echo -e "==== dest copy ==="
